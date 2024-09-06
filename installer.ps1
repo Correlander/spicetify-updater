6e0505b5e0216cd30725f679cad034cc56eac355
Alexander K. <53982700+Correlander@users.noreply.github.com>
2024-09-06 00:01:43 -0500

    Update installer.ps1
    Fix me accidentally running the MIT license in the powershell script lol
    
    Also normalized my variables at the bottom to no longer be all caps, now camel case like the others.
    
    Removed working directory because it's unnecessary
    
    Fixed the filepath to no longer be the script root, instead it goes to the thingy

--- installer.ps1
+++ installer.ps1
@@ -1,6 +1,6 @@
 Write-Output 'Spicetify Auto-Update Installer - https://github.com/Correlander/spicetify-updater'
 Write-Output "This installer will install a script to update spicetify to your computer, and add a shortcut to run it on launch to your startup folder."
-Read-Host "Press any key to continue..."
+Read-Host "Press Enter to continue..."
 
 # Define the directory and file paths
 $directoryPath = "$env:LOCALAPPDATA\SpicetifyUpdater"
@@ -18,29 +18,27 @@ if (-not (Test-Path -Path $directoryPath)) {
 # Check if the file exists
 if (-not (Test-Path -Path $filePath)) {
     # If not, download the file
-    Invoke-WebRequest -Uri $updaterFileUrl -OutFile $filePath
-    Invoke-WebRequest -Uri $licenseFileUrl -OutFile $filePath
+    Invoke-WebRequest -Uri $updaterFileUrl -OutFile Join-Path -Path $directoryPath -ChildPath "Updater.ps1"
+    Invoke-WebRequest -Uri $licenseFileUrl -OutFile Join-Path -Path $directoryPath -ChildPath "LICENSE"
     Write-Output "Downloaded $fileName to $directoryPath."
 } else {
     Write-Output "$fileName already exists in $directoryPath."
 }
 
 # Create the shortcut
-[string]$SHORTCUT_PATH = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SpicetifyUpdater.lnk"
-[string]$TARGET_PATH = "powershell.exe"
-[string]$ARGUMENTS = "-NoProfile -WindowStyle Minimized -ExecutionPolicy Bypass -File $PSScriptRoot\Updater.ps1"
-[string]$WORKING_DIRECTORY = $PSScriptRoot
+[string]$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SpicetifyUpdater.lnk"
+[string]$targetPath = "powershell.exe"
+[string]$arguments = "-NoProfile -WindowStyle Minimized -ExecutionPolicy Bypass -File $env:LOCALAPPDATA\SpicetifyUpdater\Updater.ps1"
 
-Write-Output "The installer will now create a shortcut to powershell inside the following folder: $SHORTCUT_PATH which will run Updater.ps1 on login."
-Read-Host "Press any key to continue..."
+Write-Output "The installer will now create a shortcut to powershell inside the following folder: $shortcutPath which will run Updater.ps1 on login."
+Read-Host "Press Enter to continue..."
 
 # Add it to startup
 $SHELL = New-Object -ComObject WScript.Shell
-$SHORTCUT = $SHELL.CreateShortcut($SHORTCUT_PATH)
+$SHORTCUT = $SHELL.CreateShortcut($shortcutPath)
 
-$SHORTCUT.TargetPath = $TARGET_PATH
-$SHORTCUT.Arguments = $ARGUMENTS
-$SHORTCUT.WorkingDirectory = $WORKING_DIRECTORY
+$SHORTCUT.TargetPath = $targetPath
+$SHORTCUT.Arguments = $arguments
 $SHORTCUT.Description = 'Shortcut that adds the updater script for spicetify to windows startup.'
 
 $SHORTCUT.Save()
