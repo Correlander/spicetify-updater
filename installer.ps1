d9602464082dbe0277297c1a846ffa53cff333c4
Correlander <53982700+Correlander@users.noreply.github.com>
2024-09-06 14:40:19 -0500

    Updated installer script
    Next iteration. Now v1.1, v1.0 was the first working version. Revamped it all. Might push bug fixes if necessary, but still v1.1

--- installer.ps1
+++ installer.ps1
@@ -1,45 +1,109 @@
-Write-Output 'Spicetify Auto-Update Installer - https://github.com/Correlander/spicetify-updater'
-Write-Output "This installer will install a script to update spicetify to your computer, and add a shortcut to run it on launch to your startup folder."
-Read-Host "Press Enter to continue..."
+# ANSI code variables for ease of use
+$bold = "`e[1m"
+$green = "`e[32m"
+$blue = "`e[34m"
+$reset = "`e[0m"
+
+# Fluff
+Write-Host "${bold}${green}Spicetify Installer Script v1.1${reset}"
+Write-Host "Copyright (c) 2024 Correlander - MIT License"
+Write-Host "https://github.com/Correlander/spicetify-updater]"
 
 # Define the directory and file paths
-$directoryPath = "$env:LOCALAPPDATA\SpicetifyUpdater"
-$fileName = "Updater.ps1"
-$updaterFilePath = Join-Path -Path $directoryPath -ChildPath "Updater.ps1"
-$licenseFilePath = Join-Path -Path $directoryPath -ChildPath "LICENSE"
-$updaterFileUrl = "https://raw.githubusercontent.com/Correlander/spicetify-updater/main/Updater.ps1"
-$licenseFileUrl = "https://raw.githubusercontent.com/Correlander/spicetify-updater/main/LICENSE"
+[String]$directoryPath = "$env:LOCALAPPDATA\SpicetifyUpdater"
+[String]$fileName = "Updater.ps1"
+[String]$updaterFilePath = Join-Path -Path $directoryPath -ChildPath "Updater.ps1"
+[String]$licenseFilePath = Join-Path -Path $directoryPath -ChildPath "LICENSE"
+[String]$updaterFileUrl = 'https://raw.githubusercontent.com/Correlander/spicetify-updater/main/Updater.ps1'
+[String]$licenseFileUrl = 'https://raw.githubusercontent.com/Correlander/spicetify-updater/main/LICENSE'
 
-# Check if the directory exists
-if (-not (Test-Path -Path $directoryPath)) {
-    # If not, create it -- and any non-existing parent directories
-    New-Item -Path $directoryPath -ItemType Directory -ErrorAction SilentlyContinue
+# Main block of logic, lets user make a choice and then offers them those choices again until they exit to let them set everything up
+while ($true) {
+    [String]$input
+
+    Write-Host "Enter a number to select one of the following options:"
+    Write-Host "[0] Install the Spicetify updater script."
+    Write-Host "[1] Add or remove the Spicetify updater script from startup."
+    Write-Host "[2] Exit."
+
+    [bool]$choosing = $true
+
+    while ($choosing) {
+
+        # Get user's input
+        $input = Read-Host -Prompt "Type a number then press Enter"
+
+        # Do different things base off that input
+        switch ($input) {
+            '0' {
+                # Check if the directory exists
+                if (-not (Test-Path -Path $directoryPath)) {
+                    # If not, create it -- and any non-existing parent directories
+                    New-Item -Path $directoryPath -ItemType Directory -ErrorAction SilentlyContinue
+                }
+                # Check if the file exists
+                if (-not (Test-Path -Path $updaterFilePath)) {
+                    # If not, download the file
+                    Invoke-WebRequest -Uri $updaterFileUrl -OutFile $updaterFilePath
+                    Invoke-WebRequest -Uri $licenseFileUrl -OutFile $licenseFilePath
+                    Write-Output "Downloaded $fileName to $directoryPath."
+                } else {
+                    Write-Output "$fileName already exists in $directoryPath."
+                }
+            }
+            '1'{
+                # Give them new prompts
+                Write-Host "Enter a number to select one of the following options:"
+                Write-Host "[0] Add Spicetify Updater to startup."
+                Write-Host "[1] Remove Spicetify Updater from startup."
+                Write-Host "[2] Back."
+
+                [bool]$choosing
+
+                while ($choosing) {
+                    
+                    # Get user's input
+                    $input = Read-Host -Prompt "Type a number then press Enter"
+
+                    switch ($input) {
+                        '0' {
+                            # Create the shortcut and save it in the startup folder
+                            [String]$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SpicetifyUpdater.lnk"
+                            [String]$targetPath = "powershell.exe"
+                            [String]$arguments = "-NoProfile -WindowStyle Minimized -ExecutionPolicy Bypass -File $env:LOCALAPPDATA\SpicetifyUpdater\Updater.ps1"
+
+                            $shell = New-Object -ComObject WScript.Shell
+                            $shortcut = $shell.CreateShortcut($shortcutPath)
+
+                            $shortcut.TargetPath = $targetPath
+                            $shortcut.Arguments = $arguments
+                            $shortcut.Description = 'Shortcut that adds the updater script for spicetify to windows startup.'
+
+                            $shortcut.Save()
+                        }
+                        '1' {
+                            # Remove the startup shortcut, if it exists
+                            if (Test-Path -Path $shortcutPath) {
+                                Remove-Item -Path $shortcutPath
+                            }
+                        }
+                        '2' {
+                            $choosing = $false
+                        }
+                        default {
+                            Write-Host "That is not a valid input... try again."
+                        }
+                    }
+                }
+            }
+            '2'{
+                Write-Host 'Exiting. Goodbye...'
+                Start-Sleep -Seconds 3
+                Exit
+            }
+            default {
+                Write-Host 'That is not a valid input... try again.'
+            }
+        }
+    }  
 }
-
-# Check if the file exists
-if (-not (Test-Path -Path $updaterFilePath)) {
-    # If not, download the file
-    Invoke-WebRequest -Uri $updaterFileUrl -OutFile $updaterFilePath
-    Invoke-WebRequest -Uri $licenseFileUrl -OutFile $licenseFilePath
-    Write-Output "Downloaded $fileName to $directoryPath."
-} else {
-    Write-Output "$fileName already exists in $directoryPath."
-}
-
-# Create the shortcut
-[string]$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SpicetifyUpdater.lnk"
-[string]$targetPath = "powershell.exe"
-[string]$arguments = "-NoProfile -WindowStyle Minimized -ExecutionPolicy Bypass -File $env:LOCALAPPDATA\SpicetifyUpdater\Updater.ps1"
-
-Write-Output "The installer will now create a shortcut to powershell inside the following folder: $shortcutPath which will run Updater.ps1 on login."
-Read-Host "Press Enter to continue..."
-
-# Add it to startup
-$SHELL = New-Object -ComObject WScript.Shell
-$SHORTCUT = $SHELL.CreateShortcut($shortcutPath)
-
-$SHORTCUT.TargetPath = $targetPath
-$SHORTCUT.Arguments = $arguments
-$SHORTCUT.Description = 'Shortcut that adds the updater script for spicetify to windows startup.'
-
-$SHORTCUT.Save()
