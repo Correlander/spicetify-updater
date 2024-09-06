681c20306c6c60075eb3e25071b23e1f58158aac
Correlander <53982700+Correlander@users.noreply.github.com>
2024-09-06 14:43:18 -0500

    Update Updater.ps1
    Now v1.2 as this is the second working rework of the updater.ps1 script. Bug fixes might still get pushed
    
    Now outputs to a log file in the same folder it installs to
    
    Fancier ANSI codes so the windows 11 users or windows terminal users can see nice looking text. Sucks 2 suck ig if you're on versions before that, sorry. I really wanted to be able to bold the text :3
    
    Waits for 3 seconds at the end, so it doesn't flash and look scary like a powershell script just installed a virus on ur pc whenever u launch ur computer. Sits there for a little bit to let user click and see what it is, but still starts minimized, so it isn't in the way (but not completely hidden from user).

--- Updater.ps1
+++ Updater.ps1
@@ -1,9 +1,39 @@
+# ANSI code variables for ease of use
+$bold = "`e[1m"
+$green = "`e[32m"
+$blue = "`e[34m"
+$reset = "`e[0m"
+
+# Reset log file
+Set-Content -Path ".\log.txt" -Value ""
+
+# Function to both print and log something
+function Write-Log {
+    param (
+        [string]$message
+    )
+
+    # Write to console
+    Write-Host $message
+
+    # Write to log
+    $message | Out-File -FilePath ".\log.txt" -Append
+}
+
+# This print message is because I have the window to start as minimized. If someone clicks on it, I want it to say what it's doing.
+# A non-descript powershell window running on startup is scary, even if you know you installed a script that runs on startup
+# It also is just fancy and fun ;p
+Write-Log("${bold}${green}Spicetify Updater Script${reset}")
+Write-Log("Copyright (c) 2024 Correlander - MIT License")
+Write-Log("https://github.com/Correlander/spicetify-updater]")
+
 # Back up client settings
-Write-Output "Backing up client settings..."
+Write-Log("${blue}Backing up client settings...")
 spicetify backup | Out-Null
+Write-Log("${bold}${green}OK${reset}")
 
 # Run the built in spicetify updater
-Write-Output "Checking if Spicetify is up to date..."
+Write-Log("${blue}Checking if Spicetify is up to date...${reset}")
 $updateOutput = spicetify update
 
 # Check whether or not it was already up to date
@@ -11,12 +41,16 @@ $updateOutput = spicetify update
 
 # If it wasn't up to date, apply backup
 if (-not $isUpdated) {
-    Write-Output "Spicetify was updated, restoring last backup..."
+    Write-Log("${blue}Spicetify was updated, restoring last backup...${reset}")
     spicetify backup restore | Out-Null
+    Write-Log("${bold}${green}OK${reset}")
 }
 else {
-    Write-Output "Spicetify was already up to date."
+    Write-Log("${bold}${green}Spicetify was already up to date.${reset}")
 }
 
 # Launch spotify
+Write-Log("${bold}${green}Launching Spotify and exiting updater... Goodbye.${reset}")
 & "$env:APPDATA\Spotify\Spotify.exe"
+# Wait for three seconds, so it's not just a flash
+Start-Sleep -Seconds 3
